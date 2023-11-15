72fa693bce721b80fb9040b178d7b81afee772e5
Dhiman <dibas.d87@gmail.com>
2023-11-15 09:51:09 +0600

    Added Namta java work to cloud

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i <= 10; i++)
+        {
+
+            System.out.println("5 X"+i+ "=" +(5*i));
+        }
+    }
+}
+
+/*public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("3");
+        int input = scan.nextInt();
+
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(input + " X " + i + " = " + (input * i));
+        }
+
+    }
+}*/
